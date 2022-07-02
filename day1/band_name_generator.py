a2dde5e477b5db539a6a3b88fd019be170d0e226
Irma Preldzic <66248314+IrmaFirma@users.noreply.github.com>
2022-07-02 12:57:59 +0200

    band_name_generator exercise

---
+++ day1/band_name_generator.py
@@ -0,0 +1,12 @@
+# Band Name Generator 🎤
+# Create a program that asks the user for the name of the city they grew up in
+# After that ask for their pets name
+# As a result print their city and pet's name combined
+
+# 🚀 SOLUTION: 
+print('Welcome to Band Name Generator.')
+print("What's the name of the city you grew up in?")
+name = input()
+print("What's your pet's name?")
+pet = input()
+print("Your band name could be {} {}".format(name, pet))
