015adae03eb6a23320fe195ea9301a7373f3c3c6
Irma Preldzic <66248314+IrmaFirma@users.noreply.github.com>
2022-07-02 13:00:27 +0200

    Create tip_calculator.py

---
+++ day1/tip_calculator.py
@@ -0,0 +1,17 @@
+# Tip Calculator 💸
+# Create a program that calculates the amount each person should pay
+# Including the tip
+# Use the logic for the tip calculator: https://www.calculatorsoup.com/calculators/financial/tip-calculator.php
+
+# 🚀 SOLUTION: 
+print('Welcome to Tip Calculator.')
+
+total = float(input('How much was the total bill? '))
+percent = int(input('What is your tip percentage to give? 10, 12 or 15 '))
+spliters = int(input('How many people are splitting the bill? '))
+
+bill_percent = 1 + (percent/100)
+bill = total * bill_percent
+per_person = round(bill / spliters, 2)
+
+print('Each person should pay {}'.format(per_person))
