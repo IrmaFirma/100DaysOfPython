1ed72da0f574032e190b568f96ece3d6b4f16da6
Irma Preldzic <66248314+IrmaFirma@users.noreply.github.com>
2022-07-03 18:00:55 +0200

    Create adding_evens.py

---
+++ day3/adding_evens.py
@@ -0,0 +1,12 @@
+# Adding Evens challenge 📍
+# Calculate the sum of all even numbers from 1 to 100
+
+print('These are all even numbers from 1 to 100: ')
+
+sum_even = 0
+for i in range(1, 101):
+    if i%2==0:
+        sum_even+=i
+
+print(sum_even)
+# Visit: litcode.net and @thelitcode
